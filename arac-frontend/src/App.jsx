d951512acf4158ff73f49c7aba70cebb7d68d6bf
Hamza SEZER <62917115+kiyanaa@users.noreply.github.com>
2025-09-24 14:20:14 +0300

    Update App.jsx

--- arac-frontend/src/App.jsx
+++ arac-frontend/src/App.jsx
@@ -1,6 +1,5 @@
-// App.jsx
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AracList from "./components/AracList";
 import RequestsPanel from "./components/RequestsPanel";
 import RequestVehiclePage from './components/RequestVehiclePage';
@@ -13,62 +12,58 @@ import Menu from "./Menu";
 import LoginPage from "./components/LoginPage";
 import LogoutPage from "./components/LogoutPage";
 import RegisterPage from "./components/RegisterPage";
+import ProtectedRoute from "./ProtectedRoute";
 
-function parseJwt(token) {
-  try {
-    return JSON.parse(atob(token.split(".")[1]));
-  } catch {
-    return null;
-  }
-}
-
-// AppWrapper token kontrolü yapıyor
-function AppWrapper() {
-  const navigate = useNavigate();
-  const token = localStorage.getItem("token");
-  const payload = parseJwt(token);
-  const username = payload?.username || "";
-
-  useEffect(() => {
-    if (!username) {
-      navigate("/login"); // username boş ise login sayfasına yönlendir
-    }
-  }, [username, navigate]);
-
-  return <App username={username} />;
-}
-
-function App({ username }) {
-  return (
-    <Routes>
-      <Route 
-        path="/" 
-        element={
-          <div>
-            <Menu />
-            <div>{username} Egebimtes Araç Kontrol Sistemine Hoşgeldiniz.</div>
-          </div>
-        } 
-      />
-      <Route path="/AracList" element={<AracList />} />
-      <Route path="/istekler" element={<RequestsPanel />} />
-      <Route path="/updatevehicle" element={<UpdateVehiclePage />} />
-      <Route path="/requestvehicle" element={<RequestVehiclePage />} />
-      <Route path="/addvehicle" element={<AddVehiclePage />} />
-      <Route path="/arac_istekleri" element={<RequestVehiclePanel />} />
-      <Route path="/request" element={<AddRequestPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/releasevehicle" element={<ReleaseVehiclePage />} />
-      <Route path="/logout" element={<LogoutPage />} />
-    </Routes>
-  );
-}
-
-// Router en dışta sarmalıyor
-export default function RootApp() {
+function App() {
   return (
     <Router>
-      <AppWrapper />
+      <Routes>
+        {/* Login / Register route'ları herkese açık */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected route'lar */}
+        <Route 
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <div>
+                <Menu />
+                <div>Egebimtes Araç Kontrol Sistemine Hoşgeldiniz.</div>
+              </div>
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="/AracList" element={
+          <ProtectedRoute><AracList /></ProtectedRoute>
+        } />
+        <Route path="/istekler" element={
+          <ProtectedRoute><RequestsPanel /></ProtectedRoute>
+        } />
+        <Route path="/updatevehicle" element={
+          <ProtectedRoute><UpdateVehiclePage /></ProtectedRoute>
+        } />
+        <Route path="/requestvehicle" element={
+          <ProtectedRoute><RequestVehiclePage /></ProtectedRoute>
+        } />
+        <Route path="/addvehicle" element={
+          <ProtectedRoute><AddVehiclePage /></ProtectedRoute>
+        } />
+        <Route path="/arac_istekleri" element={
+          <ProtectedRoute><RequestVehiclePanel /></ProtectedRoute>
+        } />
+        <Route path="/request" element={
+          <ProtectedRoute><AddRequestPage /></ProtectedRoute>
+        } />
+        <Route path="/releasevehicle" element={
+          <ProtectedRoute><ReleaseVehiclePage /></ProtectedRoute>
+        } />
+        <Route path="/logout" element={
+          <ProtectedRoute><LogoutPage /></ProtectedRoute>
+        } />
+      </Routes>
     </Router>
   );
 }
+
+export default App;
