c4971896634ab9635d552963465c8dd00281b6d0
Hamza SEZER <62917115+kiyanaa@users.noreply.github.com>
2025-09-24 15:18:59 +0300

    yanlış adrese yönlendirme

--- arac-frontend/src/components/RequestsPanel.jsx
+++ arac-frontend/src/components/RequestsPanel.jsx
@@ -193,7 +193,7 @@ const RequestsPanel = () => {
 
       <div className="fixed bottom-4 right-4">
         <button
-          onClick={() => navigate("/addrequest")}
+          onClick={() => navigate("/request")}
           className="flex items-center justify-center px-4 py-2 bg-green-600 text-white rounded-full hover:bg-green-700 text-sm"
         >
           <span className="material-icons">add_circle</span> İstek Ekle
