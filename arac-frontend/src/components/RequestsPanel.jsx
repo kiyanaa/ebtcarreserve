a6b67a112d3a30878a075ecfdbd6846167f1b2bb
Hamza SEZER <62917115+kiyanaa@users.noreply.github.com>
2025-09-24 13:23:38 +0300

    Update RequestsPanel.jsx

--- arac-frontend/src/components/RequestsPanel.jsx
+++ arac-frontend/src/components/RequestsPanel.jsx
@@ -131,7 +131,7 @@ const RequestsPanel = () => {
       {/* İstek Ekle Butonu */}
       <div className="fixed bottom-4 right-4">
         <button
-          onClick={() => navigate("/addrequest")}
+          onClick={() => navigate("/request")}
           className="flex items-center justify-center px-4 py-2 bg-green-600 text-white rounded-full hover:bg-green-700 text-sm"
         >
           <span className="material-icons">add_circle</span> İstek Ekle
